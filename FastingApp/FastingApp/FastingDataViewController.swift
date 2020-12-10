a33a1d767eefea673ac1f3ea5365a00bf45dcc11
cjmanaoat <cmanaoat@hotmail.com>
2020-12-10 01:23:23 -0800

    fixed structure of tab bar controller

--- FastingApp/FastingApp/FastingDataViewController.swift
+++ FastingApp/FastingApp/FastingDataViewController.swift
@@ -7,6 +7,32 @@
 
 import UIKit
 
-class FastingDataViewController: UIViewController {
+class FastingDataViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @IBOutlet weak var HeaderLabelCell: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+//        let row = indexPath.row
+        
+        
+        return cell
+    }
+    
     
 }
