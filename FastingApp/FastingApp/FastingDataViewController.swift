b046f89a175b46e07223d6527ddc248d3a933b08
cjmanaoat <cmanaoat@hotmail.com>
2020-12-09 23:57:03 -0800

    embedded first page in tab bar view controller and added second tab view controller

---
+++ FastingApp/FastingApp/FastingDataViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FastingDataViewController.swift
+//  FastingApp
+//
+//  Created by Chris Manaoat on 12/9/20.
+//
+
+import UIKit
+
+class FastingDataViewController: UIViewController {
+    
+}
